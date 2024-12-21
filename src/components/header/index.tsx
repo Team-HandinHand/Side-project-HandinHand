e1dc7d316d172e6e29f6cb39a2c0f5e05c944c34
park-chan-hui <fhfh54321@motionimaging.net>
2024-12-21 14:01:44 +0900

    chore: 라우터 초기세팅, CSS설정 없이 HEADER레이아웃 적용

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/movies">Movies</a>
+        <a href="/series">Series</a>
+      </nav>
+    </header>
+  )
+}
