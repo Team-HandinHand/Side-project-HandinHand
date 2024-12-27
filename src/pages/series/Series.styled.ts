7d95547c8741c02e18dce9d5fb57c305371587e3
park-chan-hui <fhfh54321@motionimaging.net>
2024-12-27 18:00:27 +0900

    style: series페이지 마크업

---
+++ src/pages/series/Series.styled.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const SeriesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: var(--space-medium);
+`
+export const LabelContainer = styled.div`
+  display: flex;
+  margin-bottom: var(--space-medium);
+  width: 100%;
+  @media (min-width: 1441px) {
+    width: 1440px;
+  }
+`
+
+export const PosterContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: var(--space-medium);
+  @media (min-width: 1440px) {
+    width: 1440px;
+  }
+`
+
+export const TestLabel = styled.div`
+  margin-right: 10px;
+  font-size: var(--font-medium);
+  padding: var(--space-small);
+  color: #333;
+  border: 1px solid #333;
+  border-radius: var(--border-radius-large);
+`
