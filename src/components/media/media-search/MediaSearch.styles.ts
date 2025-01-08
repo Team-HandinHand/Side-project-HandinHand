b2e8d64da0330cf82d6a240e3405bd62d77f86eb
duwlsssss <rladuwls0814@gmail.com>
2025-01-09 03:59:08 +0900

    style: 검색 컴포넌트 위치 수정 (#52)

--- src/components/media/media-search/MediaSearch.styles.ts
+++ src/components/media/media-search/MediaSearch.styles.ts
@@ -2,8 +2,13 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   margin: var(--space-medium) auto;
+  position: relative;
 `
 
 export const NoResultText = styled.p`
   color: var(--color-light-gray);
+  position: absolute;
+  left: 50%;
+  margin-top: var(--space-xxlarge);
+  transform: translateX(-50%);
 `
