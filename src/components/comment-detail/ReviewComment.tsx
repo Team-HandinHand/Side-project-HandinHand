3b664f548dab230ddc0868bf481a146742112fbb
park-chan-hui <fhfh54321@motionimaging.net>
2025-01-24 16:51:19 +0900

    feat: 수정, 삭제 로직에 toast 추가 및 댓글 삭제 시 comments detail페이지일 경우 이전 페이지로 이동하도록 변경

--- src/components/comment-detail/ReviewComment.tsx
+++ src/components/comment-detail/ReviewComment.tsx
@@ -1,9 +1,12 @@
 import StarRating from '../common-ui/star-rating/StarRating'
 import * as S from './CommentDetail.styled'
 import { Button } from '../common-ui/button/Button'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Input } from '../common-ui/input/Input'
 import { Comment } from '@/types/commentDetail'
+import { useCommentDelete } from '@/hooks/mutations/useCommentDelete'
+import { useCommentEdit } from '@/hooks/mutations/useCommentEdit'
+import { toastError } from '@/utils/toast'
 
 export const ReviewComment = ({
   commentData
@@ -11,9 +14,46 @@ export const ReviewComment = ({
   commentData: Comment | undefined
 }) => {
   const [isReadOnly, setIsReadOnly] = useState(true)
+  const commentRef = useRef<HTMLInputElement>(null)
+  const [commentValue, setCommentValue] = useState(commentData?.comment || '')
+
+  const { updateCommentMutation } = useCommentEdit({
+    comment_id: commentData?.comment_id || '',
+    newComment: commentRef.current?.value || ''
+  })
+  const { deleteCommentMutation } = useCommentDelete(
+    commentData?.comment_id || ''
+  )
+
+  const handelDeleteComment = () => {
+    if (window.confirm('선택한 댓글을을 삭제하시겠습니까?')) {
+      deleteCommentMutation()
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (commentRef.current) {
+      setCommentValue(e.target.value)
+      commentRef.current.value = e.target.value
+    }
+  }
 
   const handelEditMode = () => {
     setIsReadOnly(!isReadOnly)
+    if (!isReadOnly) {
+      if (commentRef.current?.value === '') {
+        setCommentValue(commentData?.comment || '')
+      }
+    }
+  }
+  const handleSubmit = () => {
+    if (commentRef.current?.value === '') {
+      toastError('댓글을 입력해주세요.')
+      return
+    } else {
+      updateCommentMutation()
+      setIsReadOnly(!isReadOnly)
+    }
   }
 
   if (!commentData) {
@@ -42,7 +82,7 @@ export const ReviewComment = ({
             <Button
               size="small"
               fontSize="12px"
-              onClick={handelEditMode}>
+              onClick={handleSubmit}>
               수정 완료
             </Button>
           </div>
@@ -58,7 +98,8 @@ export const ReviewComment = ({
             <Button
               color="transparent"
               size="small"
-              fontSize="12px">
+              fontSize="12px"
+              onClick={handelDeleteComment}>
               삭제
             </Button>
           </div>
@@ -68,8 +109,10 @@ export const ReviewComment = ({
         <p>평가</p>
         <Input
           readOnly={isReadOnly}
-          value={commentData.comment || ''}
+          value={commentValue}
           type="textarea"
+          ref={commentRef}
+          onChange={handleChange}
         />
       </S.CommentSection>
     </S.ReviewCommentContainer>
