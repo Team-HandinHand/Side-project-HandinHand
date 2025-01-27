9eb02a173b0baf012bf4a5c5bb78052729cbecc0
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-01-28 03:18:11 +0900

    refactor: 쿼리키 변경 #86

--- src/hooks/queries/useFetchUserComment.ts
+++ src/hooks/queries/useFetchUserComment.ts
@@ -6,7 +6,7 @@ import { useQuery } from '@tanstack/react-query'
 
 export default function useFetchUserComment(typedId: number) {
   const { data: commentsData } = useQuery<ICommentItemProps[] | undefined>({
-    queryKey: ['userComment'],
+    queryKey: ['userComment', typedId],
     queryFn: () => getComments(typedId),
     staleTime: 1000 * 60 * 5
   })
